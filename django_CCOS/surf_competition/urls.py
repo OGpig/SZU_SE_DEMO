d6589ffd0ac4ab7639cd3556ac75b41e1ddd3201
OGpig <130419821+OGpig@users.noreply.github.com>
2024-12-09 16:11:52 +0800

    Branch zcy (#7)
    * 12.7.19
    
    * 12.9.16

--- django_CCOS/surf_competition/urls.py
+++ django_CCOS/surf_competition/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import show_competition,filter_competition
+from .views import show_competition,filter_competition,show_competition_detail
 
 name='surf_competition'
 urlpatterns = [
     path('', show_competition),
     path('filter_competition/', filter_competition,name='filter_competition'),
+    path('competition_detail/', show_competition_detail,name='competition_detail'),
 ]
