7562d702bdb64fb2d7d8409190c496f402768370
LAPTOP-O9IUFKSK <2401135663@qq.com>
2024-12-10 23:42:54 +0800

    12.10.23

--- django_CCOS/contactus/views.py
+++ django_CCOS/contactus/views.py
@@ -26,11 +26,23 @@ def submit_contact_form(request):
         competitionform = request.POST.get('competitionform')
         feedbackform = request.POST.get('feedbackform')
         volunteerapplicationform = request.POST.get('volunteerapplicationform')
+        # 检查 contactus_id 是否为 10 位整数
+        try:
+            contactus_id = int(contactus_id)  # 转换为整数
+            if len(str(contactus_id)) != 10:
+                return JsonResponse({"message": "填表编号必须是 10 位整数"}, status=400)
+        except ValueError:
+            return JsonResponse({"message": "填表编号必须是有效的整数"}, status=400)
+        
         # 解析日期字段，确保日期格式为 'YYYY-MM-DD'
         try:
             date = datetime.strptime(date_str, '%Y-%m-%d').date()  # 转换为 date 对象
         except ValueError:
             return JsonResponse({"message": "日期格式不正确，必须为 'YYYY-MM-DD'"}, status=400)
+        
+        # 检查邮箱长度是否小于 30 位
+        if email and len(email) >= 30:
+            return JsonResponse({"message": "邮箱长度不能超过 30 个字符"}, status=400)
 
         # 创建新的 Contactus 实例并保存到数据库
         new_contact = Contactus(
