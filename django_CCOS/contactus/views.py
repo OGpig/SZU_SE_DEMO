5122e17f40f06911b52fcabdf7fc9cab19875699
OGpig <130419821+OGpig@users.noreply.github.com>
2024-12-10 15:08:24 +0800

    12.10.12 (#10)
    Co-authored-by: LAPTOP-O9IUFKSK <2401135663@qq.com>

--- django_CCOS/contactus/views.py
+++ django_CCOS/contactus/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from .models import Contactus
+from .models import Consult
+from .models import Feedbackform
+from .models import Volunteerrecruitment
 from datetime import datetime
 
 # Create your views here.
@@ -14,10 +17,15 @@ from datetime import datetime
 def submit_contact_form(request):
     if request.method == 'POST':
         # 从请求中获取表单数据
-        contactus_id = request.POST.get('phone')  # contactus_id 对应 phone 字段
-        date_str = request.POST.get('position')  # date 对应 position 字段
-        email = request.POST.get('email')  # email 对应 email 字段
+        contactus_id = request.POST.get('contactus_id')  
+        date_str = request.POST.get('date') 
+        email = request.POST.get('email')  
 
+        sponsorshipform = request.POST.get('sponsorshipform')
+        cooperationform = request.POST.get('cooperationform')
+        competitionform = request.POST.get('competitionform')
+        feedbackform = request.POST.get('feedbackform')
+        volunteerapplicationform = request.POST.get('volunteerapplicationform')
         # 解析日期字段，确保日期格式为 'YYYY-MM-DD'
         try:
             date = datetime.strptime(date_str, '%Y-%m-%d').date()  # 转换为 date 对象
@@ -32,6 +40,29 @@ def submit_contact_form(request):
         )
         new_contact.save()
 
+        #创建新的 Consult 实例并保存到数据库
+        new_consult = Consult(
+            contactus_id=contactus_id,
+            date=date,
+            sponsor_consultation=sponsorshipform,
+            cooperation_consultation=cooperationform,
+            competition_consultation=competitionform
+        )
+        new_consult.save()
+        #创建新的 Feedbackform 实例并保存到数据库
+        new_feedbackform = Feedbackform(
+            contactus_id=contactus_id,
+            date=date,
+            feedbackinformation=feedbackform 
+        )
+        new_feedbackform.save()
+        #创建新的 Volunteerrecruitment 实例并保存到数据库
+        new_volunteerrecruitment = Volunteerrecruitment(
+            contactus_id=contactus_id,
+            date=date,
+            recruitmentrequirements=volunteerapplicationform
+        )
+        new_volunteerrecruitment.save()
         # 返回成功响应
         return JsonResponse({"message": "信息提交成功"})
 
